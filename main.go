7c9ca34d78729fa1419549a016bcacbfef347920
Jan Kvapil <34266537+jkvapil@users.noreply.github.com>
2024-12-05 10:27:10 +0100

    Add CCM service

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"time"
 )
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
@@ -19,12 +20,28 @@ func getHello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "At /hello")
 }
 
+func (ccm *CCMService) getOrganisations(w http.ResponseWriter, r *http.Request) {
+	type response struct {
+		Name string `json:"name"`
+	}
+
+	io.WriteString(w, "At /organisations")
+}
+
+type CCMService struct {
+	serviceURL string
+}
+
 func main() {
+	ccm := CCMService{
+		serviceURL: "https://apigw.integration.resurs.com/api/customer_manager_service",
+	}
 	router := http.NewServeMux()
 	router.HandleFunc("GET /", getRoot)
 	router.HandleFunc("GET /hello", getHello)
+	router.HandleFunc("GET /organisations", ccm.getOrganisations)
 
-	err := http.ListenAndServe(":8080", router)
+	err := http.ListenAndServe(":8080", RequestLoggerMiddleware(router))
 
 	if errors.Is(err, http.ErrServerClosed) {
 		fmt.Printf("Server closed")
@@ -34,6 +51,9 @@ func main() {
 	}
 }
 
-func testMiddleware(handler func(http.ResponseWriter, *http.Request)) {
-
+func RequestLoggerMiddleware(next http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("%s: %s %s\n", time.Now().Format("2006-01-02 15:04:05.000000000"), r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	}
 }
